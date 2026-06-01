b601632163db577e206d1aa7aaa6228cfc7dd00e
vedsu <90701239+vedsu@users.noreply.github.com>
2026-06-01 17:54:26 +0530

    Refactor app.run to use multi-line arguments

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+# app.py
+
 from app import app
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(
+        host="0.0.0.0",
+        port=8080,
+        debug=False
+    )
